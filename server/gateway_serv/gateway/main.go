6b61e81fb9e9f5d3aa7eb2981bb91780d741154d
haomiao000 <1843471489@qq.com>
2024-12-22 16:30:01 +0800

    Update

--- server/gateway_serv/gateway/main.go
+++ server/gateway_serv/gateway/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/haomiao000/rebuildSDP/server/gateway_serv/gateway/biz/router"
-
+	// "github.com/haomiao000/DY/server/gateway_serv/gateway/biz/router"
 	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	r := gin.Default()
 	router.InitRouter(r)
+	
 	r.Run(":8080")
 }
