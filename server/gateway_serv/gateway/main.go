a95a3d978bbb23ea9c898abad21b935f49208be9
haomiao000 <1843471489@qq.com>
2024-12-22 16:38:21 +0800

    update

--- server/gateway_serv/gateway/main.go
+++ server/gateway_serv/gateway/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/haomiao000/rebuildSDP/server/gateway_serv/gateway/biz/router"
+	// "github.com/haomiao000/rebuildSDP/server/gateway_serv/gateway/biz/router"
 	// "github.com/haomiao000/DY/server/gateway_serv/gateway/biz/router"
+	"github.com/haomiao000/rebuildSDP/server/gateway_serv/gateway/biz/router"
 	gin "github.com/gin-gonic/gin"
 )
 
